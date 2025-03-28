6571d2816344b5f3e4ae8419eb058c2a76dceeb1
Xelleet <ererenyeager@yandex.ru>
2025-03-28 21:30:02 +0300

    some little changes

--- Back/todolist_butBetter/todolist/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import User
-from .serialiser import UserSerializer
-
-class MessageCreateView(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
